70861004b25b64c9c3731e1f6c13a41f5d63a3cf
kavita1666 <kavitajatwar970@gmail.com>
2025-01-28 19:41:36 +0530

    added edit delete reply to comments

--- src/components/CommentsSection/CommentCard/CommentCard.js
+++ src/components/CommentsSection/CommentCard/CommentCard.js
@@ -1,36 +1,107 @@
 import React, { useState } from "react";
 import "./CommentCard.css";
 
-export const CommentCard = ({ comment }) => {
+export const CommentCard = ({ comment, commentsData, setCommentsData }) => {
   const [replyBoxOpen, setReplyBoxOpen] = useState(false);
   const [replyContent, setReplyContent] = useState("");
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  const handleReply = () => {
-    setReplyBoxOpen(!replyBoxOpen);
+  // function with recursion
+  const handleInsertReply = (replyId, replyData) => {
+    const newComments = replyData.map((item) => {
+      if (item.id === replyId) {
+        const lastDigit = item.id.split(".").pop();
+        return {
+          ...item,
+          replies: [...item.replies, { id: item.id + "." + lastDigit + 1, content: replyContent, replies: [] }],
+        };
+      } else if (item.replies && item.replies.length > 0) {
+        return {
+          ...item,
+          replies: handleInsertReply(replyId, item.replies),
+        };
+      }
+      return item;
+    });
+    return newComments;
+  };
+
+  // handle reply
+  const handleSubmitReply = (replyId) => {
+    if (replyContent.trim().length > 0) {
+      console.log(replyId, handleInsertReply(replyId, commentsData));
+      setCommentsData((prevComments) => handleInsertReply(replyId, prevComments));
+      setReplyBoxOpen(!replyBoxOpen);
+      setReplyContent("");
+    }
+  };
+
+  const handleEditCommentRecursion = (commentId, data) => {
+    const newComments = data.map((item) => {
+      if (item.id === commentId) {
+        return {
+          ...item,
+          content: replyContent,
+        };
+      } else if (item.replies && item.replies.length > 0) {
+        return {
+          ...item,
+          replies: handleEditCommentRecursion(commentId, item.replies),
+        };
+      }
+      return item;
+    });
+    return newComments;
+  };
+
+  //handle Edit
+  const handleEditComment = (commentId) => {
+    setReplyBoxOpen(true);
+    setReplyContent(comment.content);
+    setIsEditMode(true);
+    console.log("---here ", replyContent, comment.content);
+    if (replyContent !== comment.content) {
+      setCommentsData((prevComments) => handleEditCommentRecursion(commentId, prevComments));
+    }
+  };
+
+  const handleDeleteCommentRecursion = (commentId, data) => {
+    return data.reduce((acc, comment) => {
+      if (comment.id === commentId) {
+        return acc;
+      } else if (comment.replies && comment.replies.length > 0) {
+        comment.replies = handleDeleteCommentRecursion(commentId, comment.replies);
+      }
+      return [...acc, comment];
+    }, []);
+  };
+
+  // handle Delete
+  const handleDeleteComment = (commentId) => {
+    setCommentsData((prevComments) => handleDeleteCommentRecursion(commentId, prevComments));
   };
 
   return (
     <>
       <div className="comment-with-reply">
         <h3>{comment.content}</h3>
-        <p>Votes: 0</p>
-        <p>{Date.now()}</p>
+        <p>Id: {comment.id}</p>
         <div>
-          <button className="button" onClick={handleReply}>
+          <button className="button" onClick={() => setReplyBoxOpen(!replyBoxOpen)}>
             {replyBoxOpen ? "Hide Reply" : "Reply"}
           </button>
-          <button className="button" onClick={() => {}}>
+          <button className="button" onClick={() => handleEditComment(comment.id)}>
             Edit
           </button>
-          <button className="button" onClick={() => {}}>
+          <button className="button" onClick={() => handleDeleteComment(comment.id)}>
             Delete
           </button>
         </div>
         {replyBoxOpen && (
           <div className="reply-box">
             <textarea value={replyContent} onChange={(e) => setReplyContent(e.target.value)} className="reply-input" />
-            <button onClick={() => {}} className="button submit-reply">
-              Submit
+            <button onClick={() => (isEditMode ? handleEditComment(comment.id) : handleSubmitReply(comment.id))} className="button submit-reply">
+              {isEditMode ? "Edit" : "Submit"}
             </button>
           </div>
         )}
@@ -40,9 +111,9 @@ export const CommentCard = ({ comment }) => {
         comment.replies.length > 0 &&
         comment.replies.map((reply) => {
           return (
-            <li style={{ listStyleType: "none", paddingLeft: "20px" }}>
-              <CommentCard comment={reply} />
-            </li>
+            <div style={{ listStyleType: "none", paddingLeft: "20px" }}>
+              <CommentCard comment={reply} key={reply.id} commentsData={reply.replies} setCommentsData={setCommentsData} />
+            </div>
           );
         })}
     </>
--- src/components/CommentsSection/CommentsSection.js
+++ src/components/CommentsSection/CommentsSection.js
@@ -2,44 +2,42 @@ import React, { useState } from "react";
 import "./CommentsSection.css";
 import { CommentCard } from "./CommentCard/CommentCard";
 
-const sampleData = [
-  {
-    id: "1",
-    content: "comment 1",
-    replies: [
-      {
-        id: "1.1",
-        content: "reply 1.1",
-        replies: [
-          {
-            id: "1.1.1",
-            content: "reply 1.1.1",
-            replies: [],
-          },
-          {
-            id: "1.1.2",
-            content: "reply 1.1.2",
-            replies: [],
-          },
-        ],
-      },
-      {
-        id: "1.2",
-        content: "reply 1.2",
-        replies: [],
-      },
-    ],
-  },
-  {
-    id: "2",
-    content: "comment 2",
-    replies: [],
-  },
-];
+// const sampleData = [
+//   {
+//     id: "1",
+//     content: "comment 1",
+//     replies: [
+//       {
+//         id: "1.1",
+//         content: "reply 1.1",
+//         replies: [
+//           {
+//             id: "1.1.1",
+//             content: "reply 1.1.1",
+//             replies: [],
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
 
 export const CommentsSection = () => {
-  const [commentsData, setCommentsData] = useState(sampleData);
+  const [commentsData, setCommentsData] = useState([]);
   const [value, setValue] = useState("");
+
+  const handleAddComment = () => {
+    if (!value) return;
+    else {
+      const newComments = {
+        id: `${commentsData.length + 1}`,
+        content: value,
+        replies: [],
+      };
+      setCommentsData([...commentsData, newComments]);
+      setValue("");
+    }
+  };
 
   return (
     <div className="comments-section-container">
@@ -47,14 +45,14 @@ export const CommentsSection = () => {
       <div className="comments-inner-container">
         <div className="input-container">
           <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="input-field" placeholder="type something here..." />
-          <button type="button" className="add-button" onClick={() => {}}>
+          <button type="button" className="add-button" onClick={() => handleAddComment()}>
             ADD
           </button>
         </div>
         <div className="display-comments">
           {commentsData &&
             commentsData.map((comment) => {
-              return <CommentCard comment={comment} key={comment.id} />;
+              return <CommentCard comment={comment} key={comment.id} commentsData={commentsData} setCommentsData={setCommentsData} />;
             })}
         </div>
       </div>
