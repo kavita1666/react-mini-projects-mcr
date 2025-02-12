90b2cb628310ac3676ab75c2a9226973cd0ffa24
kavita1666 <kavitajatwar970@gmail.com>
2025-02-12 22:20:46 +0530

    progress bar created

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProgressBar.css";
 
 export const ProgressBar = () => {
+  const [percentageComplete, setPercentageComplete] = useState(0);
+
+  useEffect(() => {
+    if (percentageComplete < 100) {
+      const interval = setInterval(() => {
+        setPercentageComplete((prev) => prev + 1);
+      }, 100);
+
+      return () => clearInterval(interval);
+    } 
+    // else if (percentageComplete === 100) {
+    //   alert("Progress bar is complete");
+    // }
+  }, [percentageComplete]);
+
   return (
-    <div>
+    <div className="progress-bar-container">
       <h1>ProgressBar</h1>
+      <div className="progress-bar">
+        <span className="percentage-complete">{percentageComplete + "%"}</span>
+        <div style={{ width: `${percentageComplete}%` }} role="progressbar" aria-valuemin={0} aria-valuemax={100} aria-valuenow={percentageComplete} />
+      </div>
     </div>
   );
 };
